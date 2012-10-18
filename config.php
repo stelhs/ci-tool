53dc9d2346b44f1d099419918f601df1b68ea2c0
Dmitriy Gorokh <dmitriy.gorokh@promwad.com>
2012-10-18 18:30:55 +0300

    Modify configs

--- config.php
+++ config.php
@@ -1,20 +1,20 @@
 <?php
-$_CONFIG['ci_dir'] = '/home/stelhs/projects/ci/ci/';
-$_CONFIG['project_dir'] = '/home/stelhs/projects/ci/projects/';
+$_CONFIG['ci_dir'] = '/opt/ci-tool/';
+$_CONFIG['project_dir'] = '/var/opt/ci-projects/';
 $_CONFIG['web'] = '/home/stelhs/projects/ci/web/';
 $_CONFIG['ci_repo'] = 'ci-tool.git';
 $_CONFIG['ci_projects_repo'] = 'ci-projects.git';
 $_CONFIG['ci_servers'] = array(
                                 array('host' => '192.168.10.244',
                                       'port' => 22,
-                                      'login' => 'stelhs',
+                                      'login' => 'ci-tool',
                                       'role' => 'web',
                                       'description' => 'xz',
                                 ),
 
-                                array('host' => 'localhost',
+                                array('host' => '192.168.10.2',
                                       'port' => 22,
-                                      'login' => 'stelhs',
+                                      'login' => 'ci-tool',
                                       'role' => 'build',
                                       'description' => 'xz',
                                 ),
