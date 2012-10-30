d9ee8ca1dbb4b254a8673c4cba4bf17e5cf6cd91
michail kurachkin <michail.kurachkin@promwad.com>
2012-10-30 15:05:45 +0300

    nothing really special

--- lib.php
+++ lib.php
@@ -110,6 +110,8 @@ function run_remote_cmd(array $server, $cmd, $fork = false)
 
     $cmd = str_replace('$', '\$', $cmd);
 
+    dump('$fork = ' . $fork);
+
     if ($fork == true)
     {
         $pid = pcntl_fork();
