bb25fcc2dc6ee049c85f01bcffc8855d5221b4fc
michail kurachkin <michail.kurachkin@promwad.com>
2012-11-06 11:12:44 +0300

    nothing really special

--- Target.php
+++ Target.php
@@ -116,10 +116,12 @@ class Target
             if (!$matches)
                 continue;
 
-            dump($matches[1]);
+
 
             $session_date = new CiDateTime();
             $session_date->from_string($matches[1]);
+
+            dump($session_date->to_string());
 
             $session = new Session($this, $this->dir . '/' . $dir_name, $session_date, $matches[2]);
             $this->add_session($session);
