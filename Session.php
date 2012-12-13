e92a7f3fcc21580cf7854693f1de6050de22d849
michail kurachkin <michail.kurachkin@promwad.com>
2012-12-13 15:04:07 +0300

    nothing really special

--- Session.php
+++ Session.php
@@ -475,8 +475,7 @@ class Session
         /*
          * generate html filelfds
          */
-        $tpl = new Tpl($_CONFIG['ci_dir'] . '/templates/report.html');
-        $tpl->enable_comments = false;
+        $tpl = new Tpl($_CONFIG['ci_dir'] . '/templates/report.html', array(), TPL_INTERPRETER_MODE, '', false);
         $tpl->assign(0, $report_data);
         if ($build_result_paths)
             foreach($build_result_paths as $path)
@@ -488,8 +487,7 @@ class Session
         /*
          * generate and send email
          */
-        $tpl = new Tpl($_CONFIG['ci_dir'] . '/templates/email_report.html');
-        $tpl->enable_comments = false;
+        $tpl = new Tpl($_CONFIG['ci_dir'] . '/templates/email_report.html', array(), TPL_INTERPRETER_MODE, '', false);
         $subject_template = trim($tpl->load_block('build_result_subject'));
         $email_template = $tpl->load_block('build_result');
 
