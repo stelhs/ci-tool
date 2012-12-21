bf7c1fdcbf96f77c1e3d7dcd8aff292d9eabf0e3
michail kurachkin <michail.kurachkin@promwad.com>
2012-12-21 13:50:28 +0300

    nothing really special

---
+++ run_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function error() {
+  local PARENT_LINENO="$1"
+  local MESSAGE="$2"
+  local CODE="${3:-1}"
+  if [[ -n "$MESSAGE" ]] ; then
+    echo "Error on or near line ${PARENT_LINENO}: ${MESSAGE}; exiting with status ${CODE}"
+  else
+    echo "Error on or near line ${PARENT_LINENO}; exiting with status ${CODE}"
+  fi
+  exit "${CODE}"
+}
+trap 'error ${LINENO}' ERR
+
+script=$1
+shift 1
+./$script $@
