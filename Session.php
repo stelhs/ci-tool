62e68381c8a29690fbaed977ef981e33b74fe8b6
michail kurachkin <michail.kurachkin@promwad.com>
2012-12-21 15:46:35 +0300

    nothing really special

--- Session.php
+++ Session.php
@@ -306,11 +306,11 @@ class Session
 
         create_file($this->dir . '/.pid', getmypid());
 
-        $ret = run_cmd('set -e; cd ' . $this->dir . ' && ' .
+        $ret = run_cmd('cd ' . $this->dir . ' && ' .
             $_CONFIG['ci_dir'] . "/run_script.sh " .
             $this->target->get_dir() . '/' . $bash_file . ' ' . $args .
             ($log_file ? (' 2>&1 | tee -a ' . $this->dir . '/' . $log_file) : '') . ";" .
-            'exit $?',
+            'exit $PIPESTATUS',
             false, '', true);
 
         dump($ret);
