32895e639912874b6882b96fc23887b0637c7073
michail kurachkin <michail.kurachkin@promwad.com>
2012-12-21 15:29:50 +0300

    nothing really special

--- Session.php
+++ Session.php
@@ -306,7 +306,7 @@ class Session
 
         create_file($this->dir . '/.pid', getmypid());
 
-        $ret = run_cmd('cd ' . $this->dir . ' && ' .
+        $ret = run_cmd('set -e; cd ' . $this->dir . ' && ' .
             $_CONFIG['ci_dir'] . "/run_script.sh " .
             $this->target->get_dir() . '/' . $bash_file . ' ' . $args .
             ($log_file ? (' 2>&1 | tee -a ' . $this->dir . '/' . $log_file) : '') . ";" .
