a590bd27de888cd445f4a875f26388d56689e4f3
michail kurachkin <michail.kurachkin@promwad.com>
2013-01-08 16:00:59 +0300

    nothing really special

--- lib.php
+++ lib.php
@@ -95,6 +95,7 @@ function run_cmd($cmd, $fork = false, $stdin_data = '', $print_stdout = false)
         1 => array("pipe", "w"),
     );
 
+    
     $fd = proc_open($cmd . ' 2>&1;', $descriptorspec, $pipes);
     if ($fd == false)
         throw new Exception("proc_open() error in run_cmd()");
